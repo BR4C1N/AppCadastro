7e281a8be69621587e31751bfde249d27a0f27c5
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2022-10-14 09:37:06 -0400

    V1.4 - Alteração do metodo para privado

--- app/src/main/java/com/software/appdecadastro/activities/MainActivity.java
+++ app/src/main/java/com/software/appdecadastro/activities/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         acoesComponentes();
     }
 
-    public void acoesComponentes(){
+    private void acoesComponentes(){
         botaoProdutos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
