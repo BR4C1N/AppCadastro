3552df33bbb9caa24d3b80cca9a9aead8edab3ca
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2022-10-14 14:32:25 -0400

    V3.1 - Add classe do banco de dados

--- app/src/main/java/com/software/appdecadastro/dataBase/DBHelper.java
+++ app/src/main/java/com/software/appdecadastro/dataBase/DBHelper.java
@@ -1,2 +1,28 @@
-package com.software.appdecadastro.dataBase;public class DBHelper {
+package com.software.appdecadastro.dataBase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(@Nullable Context context){
+        super(context, "AppCadastro", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String querySQL =
+                "CREATE TABLE PRODUTOS(" +
+                        "ID INTEGER PRIMARY KEY AUTOINCREMENT" +
+                        "" +
+                        ")";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
