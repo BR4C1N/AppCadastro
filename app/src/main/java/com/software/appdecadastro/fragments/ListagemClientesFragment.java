536a06235a5af8725ac8eee1825379bbeaef3e1b
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2022-10-18 12:41:38 -0400

    V4.0 - Add código dos fragmentos de listagem

--- app/src/main/java/com/software/appdecadastro/fragments/ListagemClientesFragment.java
+++ app/src/main/java/com/software/appdecadastro/fragments/ListagemClientesFragment.java
@@ -2,65 +2,63 @@ package com.software.appdecadastro.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.software.appdecadastro.R;
+import com.software.appdecadastro.dataBase.ClienteDB;
+import com.software.appdecadastro.dataBase.DBHelper;
+import com.software.appdecadastro.entities.Cliente;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListagemClientesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListagemClientesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ListView listaDados;
+    static List<Cliente> listaClientes;
+    static ArrayAdapter adapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    static ClienteDB clienteDB;
 
     public ListagemClientesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListagemClientesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListagemClientesFragment newInstance(String param1, String param2) {
-        ListagemClientesFragment fragment = new ListagemClientesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listagem_clientes, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_listagem_clientes, container, false);
+
+        listaDados = (ListView) view.findViewById(R.id.listViewClientes);
+
+        DBHelper db = new DBHelper(getActivity());
+        clienteDB = new ClienteDB(db);
+
+        listaClientes = new ArrayList<>();
+        adapter = new ArrayAdapter(getActivity(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listaClientes);
+        listaDados.setAdapter(adapter);
+
+        listarDadosClientes();
+
+        return view;
     }
+
+    protected static void listarDadosClientes() {
+        clienteDB.listar(listaClientes);
+        adapter.notifyDataSetChanged();
+    }
+
 }
--- app/src/main/java/com/software/appdecadastro/fragments/ListagemProdutosFragment.java
+++ app/src/main/java/com/software/appdecadastro/fragments/ListagemProdutosFragment.java
@@ -7,60 +7,55 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.software.appdecadastro.R;
+import com.software.appdecadastro.dataBase.DBHelper;
+import com.software.appdecadastro.dataBase.ProdutoDB;
+import com.software.appdecadastro.entities.Produto;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListagemProdutosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListagemProdutosFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ListView listaDados;
+    static List<Produto> listaProdutos;
+    static ArrayAdapter adapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    static ProdutoDB produtoDB;
 
     public ListagemProdutosFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListagemProdutosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListagemProdutosFragment newInstance(String param1, String param2) {
-        ListagemProdutosFragment fragment = new ListagemProdutosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listagem_produtos, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_listagem_produtos, container, false);
+
+        listaDados = view.findViewById(R.id.listViewProdutos);
+
+        DBHelper db = new DBHelper(getActivity());
+        produtoDB = new ProdutoDB(db);
+
+        listaProdutos = new ArrayList<>();
+        adapter = new ArrayAdapter(getActivity(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listaProdutos);
+        listaDados.setAdapter(adapter);
+
+        listarDadosProdutos();
+
+        return view;
     }
+
+    protected static void listarDadosProdutos() {
+        produtoDB.listar(listaProdutos);
+        adapter.notifyDataSetChanged();
+    }
+
 }
